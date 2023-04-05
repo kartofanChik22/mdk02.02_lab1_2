7c1a095aa11020d2b7bb147d68ee04a25e8b8473
kartofan <anton5veselov459@gmail.com>
2023-04-05 13:46:58 +0300

    Add generate function and it's call

---
+++ generate.py
@@ -0,0 +1,6 @@
+import random
+def generate():
+    l=[]
+    for i in range(14):
+        l.append(random.randint(1,40))
+    return l
---
+++ main.py
@@ -0,0 +1,2 @@
+from generate import generate
+print(*generate())
